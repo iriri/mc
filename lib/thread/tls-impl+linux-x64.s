73da98b1db17fe48c8c8c91e02e3f2d035b03e4e
iriri <iri@konnichiwastevenspielbergde.su>
2018-09-15 01:02:40 -0700

    penguin

--- lib/thread/tls-impl+linux-x64.s
+++ lib/thread/tls-impl+linux-x64.s
@@ -2,19 +2,21 @@
 .globl _thread$tid
 thread$tid:
 _thread$tid:
-	movl	%fs:0x00, %eax
+	movl	%fs:0x0, %eax
 	ret
 
-.globl thread$settls
-.globl _thread$settls
-thread$settls:
-_thread$settls:
-	movq	%rdi, %fs:0x08
+.globl thread$tlsset
+.globl _thread$tlsset
+thread$tlsset:
+_thread$tlsset:
+	movslq	%edi, %rdi
+	movq	%rsi, 16(%fs, %rdi, 8)
 	retq
 
-.globl thread$tls
-.globl _thread$tls
-thread$tls:
-_thread$tls:
-	movq	%fs:0x08, %rax
+.globl thread$tlsget
+.globl _thread$tlsget
+thread$tlsget:
+_thread$tlsget:
+	movslq	%edi, %rdi
+	movq	16(%fs, %rdi, 8), %rax
 	retq
